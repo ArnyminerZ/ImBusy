14a76be48dc41fd9048e5cc03b698e9974b47642
Arnau Mora <arnyminer.z@gmail.com>
2022-06-20 11:54:15 +0100

    :sparkles: Added extensions
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- lib/enum/confirmation_state.dart
+++ lib/enum/confirmation_state.dart
@@ -1,5 +1,31 @@
+import 'package:flutter/material.dart';
+
 enum ConfirmationState {
   confirmed,
   unconfirmed,
   denied,
+}
+
+extension ConfirmationStateExtension on ConfirmationState {
+  String name() {
+    switch (this) {
+      case ConfirmationState.confirmed:
+        return "Confirmed";
+      case ConfirmationState.unconfirmed:
+        return "Unconfirmed";
+      case ConfirmationState.denied:
+        return "Won't come";
+    }
+  }
+
+  IconData icon() {
+    switch (this) {
+      case ConfirmationState.confirmed:
+        return Icons.check_rounded;
+      case ConfirmationState.unconfirmed:
+        return Icons.dark_mode_rounded;
+      case ConfirmationState.denied:
+        return Icons.close_rounded;
+    }
+  }
 }
