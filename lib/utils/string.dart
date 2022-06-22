6b69967e7780dac214569eae92fa08e2f4cf4d32
Arnau Mora <arnyminer.z@gmail.com>
2022-06-22 19:33:35 +0100

    :sparkles: Added `emailRegex`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -3,3 +3,5 @@ import 'package:sprintf/sprintf.dart';
 extension StringFormatExtension on String {
   String format(var arguments) => sprintf(this, arguments);
 }
+
+RegExp emailRegex = RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
